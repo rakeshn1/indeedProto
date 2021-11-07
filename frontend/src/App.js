1d210c804bf8eb7cc740416985790c600e1f5349
Chandra Lekha Mamidi <sravyaratnachandralekha.mamidi@sjsu.edu>
2021-11-06 22:33:08 -0700

    Add company snapshot page and about page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { Switch, Route } from "react-router-dom";
 import JobSeekerHome from "./components/JobSeeker/JobSeekerHome";
 import EmployerHome from "./components/Employer/EmployerHome";
 import Dashboard from "./components/Admin/Dashboard";
+import CompanyHome from "./components/JobSeeker/CompanyDetails/CompanyHome";
+import AddReview from "./components/JobSeeker/CompanyDetails/AddReview";
 import "./App.css";
 function App() {
   return (
@@ -9,6 +11,12 @@ function App() {
       <Switch>
         <Route path="/jobSeeker">
           <JobSeekerHome />
+        </Route>
+        <Route path="/companydetails">
+          <CompanyHome />
+        </Route>
+        <Route path="/company/addReview">
+          <AddReview />
         </Route>
         <Route path="/employer">
           <EmployerHome />
