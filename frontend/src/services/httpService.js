b2d7ff25efa6b6b21d5b352bfbc8f26363c0bb93
Chandra Lekha Mamidi <sravyaratnachandralekha.mamidi@sjsu.edu>
2021-11-09 15:38:59 -0800

    Add common folders.

---
+++ frontend/src/services/httpService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+axios.interceptors.response.use(null, (error) => {
+  console.log(error);
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+  if (!expectedError) {
+    alert.error("Something went wrong");
+  }
+  return Promise.reject(error);
+});
+
+function setJwt(jwt) {
+  axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+  setJwt,
+};
--- frontend/src/services/jobSeeker.js
+++ frontend/src/services/jobSeeker.js
@@ -1,1 +1,30 @@
 //  All api calls can be written here
+
+import http from "./httpService";
+
+import { apiURL } from "../config";
+
+const apiEndpoint = apiURL + "/jobSeeker";
+
+export async function addReview(review) {
+  debugger;
+  return await http.post(apiEndpoint + "/addReview", {
+    jobSeekerId: 1,
+    companyId: 1,
+    rating: review.overallRating,
+    workLifeBal: review.workLifeBal,
+    jobSecurity: review.jobSecurity,
+    management: review.management,
+    culture: review.culture,
+    benefits: review.benefits,
+    reviewSummary: review.reviewSummary,
+    review: review.review,
+    pros: review.pros,
+    cons: review.cons,
+    jobTitle: review.jobTitle,
+    jobLocation: review.jobLocation,
+    CEOApproval: review.ceoApproval,
+    howShouldIPrepare: review.tips,
+    status: 0,
+  });
+}
