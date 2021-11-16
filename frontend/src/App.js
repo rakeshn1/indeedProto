1fd17feade3acddf5cb97e0aef764342be243351
aaronmq96 <aaronnmq96@gmail.com>
2021-11-16 15:35:55 -0800

    Navbar and Landing Page css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,23 @@ import { Switch, Route } from "react-router-dom";
 import JobSeekerHome from "./components/JobSeeker/JobSeekerHome";
 import EmployerHome from "./components/Employer/EmployerHome";
 import Dashboard from "./components/Admin/Dashboard";
+import JobSeekerLandingPage from "./components/JobSeeker/JobSeekerLandingPage";
+
 import "./App.css";
+
 function App() {
   return (
     <div className="App">
+
       <Switch>
         <Route path="/jobSeeker">
           <JobSeekerHome />
         </Route>
         <Route path="/employer">
           <EmployerHome />
+        </Route>
+        <Route path="/jobSeekerLandingPage">
+          <JobSeekerLandingPage />
         </Route>
         <Route path="/admin">
           <Dashboard />
