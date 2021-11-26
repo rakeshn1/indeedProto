04bed1d84b686a8245196ee492650e6b9280ad17
HimajaChandaluri <himaja.chandaluri@gmail.com>
2021-11-25 23:54:56 -0800

    Job seeker Salaries tab frontend and backend integrated

--- kafkabackend/APIS/jobSeeker.js
+++ kafkabackend/APIS/jobSeeker.js
@@ -1,5 +1,6 @@
 const { Reviews } = require("../models/review");
-const Company = require("../models/company");
+const { SalaryReview } = require("../models/salaryReview");
+const { Company } = require("../models/company");
 
 async function addReview(body, callback) {
   try {
@@ -12,9 +13,42 @@ async function addReview(body, callback) {
   }
 }
 
+// jobSeekerId: "619f12eb7a93a10478dcae74",
+//       companyId: "619ebb183ee1aa8bb08188a0",
+//       isJobSeekerCurrentCompany: true,
+//       jobTitle: "Software Engineer",
+//       salary: 9000,
+//       yearsOfReleventexperience: 5,
+//       benefits: ["Benefit1", "Benefit2", "Benefit3"],
+
+async function addSalaryReview(msg, callback) {
+  const res = {};
+  try {
+    const salaryReview = new SalaryReview({
+      jobSeekerId: msg.body.jobSeekerId,
+      companyId: msg.body.companyId,
+      isJobSeekerCurrentCompany: msg.body.isJobSeekerCurrentCompany,
+      endDate: msg.body.endDate ? msg.body.endDate : undefined,
+      jobTitle: msg.body.jobTitle,
+      salary: msg.body.salary,
+      yearsOfReleventexperience: msg.body.yearsOfReleventexperience,
+      banefits: msg.body.benefits,
+    });
+
+    await salaryReview.save();
+    res.status = 200;
+    res.data = "Succesfullt added salaries review";
+    callback(null, res);
+  } catch (ex) {
+    res.status = 500;
+    res.data = ex;
+    callback(null, res);
+  }
+}
+
 async function getJobSearchResults(body, callback) {
   try {
-    console.log("body", body)
+    console.log("body", body);
 
     callback(null, "Sent Results");
   } catch (ex) {
@@ -29,12 +63,17 @@ handle_request = (msg, callback) => {
     console.log("Kafka side1");
     addReview(msg, callback);
   }
-
   if (msg.path === "getJobSearchResults") {
     delete msg.path;
-    console.log("HERE")
+    console.log("HERE");
     console.log("Kafka side1");
     getJobSearchResults(msg, callback);
+  }
+  if (msg.path === "addSalaryReview") {
+    delete msg.path;
+    console.log("HERE");
+    console.log("Kafka side1");
+    addSalaryReview(msg, callback);
   }
 };
 
