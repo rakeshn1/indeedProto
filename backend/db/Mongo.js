7f8ee039c9efd878a62f6769640091e6f72cc4a4
aaronmq96 <aaronmq96@gmail.com>
2021-12-13 19:54:42 -0800

    removed mongodb url

--- backend/db/Mongo.js
+++ backend/db/Mongo.js
@@ -6,7 +6,7 @@ dotenv.config({ path: __dirname + "/./../config/config.env" });
 const DB = process.env.DATABASE;
 mongoose
   .connect(
-    "mongodb+srv://admin:root@cluster0.uvvol.mongodb.net/indeedClone?retryWrites=true&w=majority",
+    "",
     {
       useNewUrlParser: true,
       // useCreateIndex: true,
